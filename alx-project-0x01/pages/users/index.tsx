1a17069c410fd8c5c2e9fc065759893972b7aa6c
Musa Ibrahim <musaibrahim1105@gmail.com>
2025-07-26 15:25:14 +0100

    task 0 n 1 done

---
+++ alx-project-0x01/pages/users/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from "@/components/layout/Header";
+
+function index() {
+  return (
+    <div>
+        <Header />
+      <h2>user index</h2>
+    </div>
+  )
+}
+
+export default index
